47ebfb14ff398b177c477e1514c82b6602d7179c
MarcosAAlbanoJunior <marcosaalbanojunior@gmail.com>
2026-06-17 19:11:19 -0300

    first commit

---
+++ src/main/java/io/whyjvm/playground/WhyjvmPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package io.whyjvm.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhyjvmPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhyjvmPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/whyjvm/playground/WhyjvmPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package io.whyjvm.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhyjvmPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
